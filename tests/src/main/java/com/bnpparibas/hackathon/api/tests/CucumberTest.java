759ddb1cee4736b0334de4ef2814d33821d223a7
amaroaxians <amaro.bica@axiansms.pt>
2020-03-14 11:01:06 +0000

    cucumber initial

---
+++ tests/src/main/java/com/bnpparibas/hackathon/api/tests/CucumberTest.java
@@ -0,0 +1,6 @@
+package com.bnpparibas.hackathon.api.tests;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+public class CucumberTest {
+}
---
+++ tests/src/test/java/com/bnpparibas/hackathon/api/tests/TestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnpparibas.hackathon.api.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

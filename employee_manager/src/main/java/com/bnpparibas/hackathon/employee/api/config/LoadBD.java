ee89852f3e324f9857b9f4f6cf66db8feaa42eb2
Joaquim <rasarrad@gmail.com>
2020-03-14 13:48:13 +0000

    Test data upadate

--- employee_manager/src/main/java/com/bnpparibas/hackathon/employee/api/config/LoadBD.java
+++ employee_manager/src/main/java/com/bnpparibas/hackathon/employee/api/config/LoadBD.java
@@ -21,27 +21,27 @@ public class LoadBD implements CommandLineRunner {
     	emp1.setFirstName("firstName 1");
     	emp1.setId(1L);
     	emp1.setLastName("lastName 1");
-    	employeeRepository.save(emp1);
+    	this.employeeRepository.save(emp1);
     	
     	Employee emp2 = new Employee();
     	emp2.setEmailId("emailId 2");
     	emp2.setFirstName("firstName 2");
     	emp2.setId(2L);
     	emp2.setLastName("lastName 2");
-    	employeeRepository.save(emp2);
+    	this.employeeRepository.save(emp2);
     	
     	Employee emp3 = new Employee();
     	emp3.setEmailId("emailId 3");
     	emp3.setFirstName("firstName 3");
     	emp3.setId(3L);
     	emp3.setLastName("lastName 3");
-    	employeeRepository.save(emp3);
+    	this.employeeRepository.save(emp3);
     	
     	Employee emp4 = new Employee();
     	emp4.setEmailId("emailId 4");
     	emp4.setFirstName("firstName 4");
     	emp4.setId(4L);
     	emp4.setLastName("lastName 4");
-    	employeeRepository.save(emp4);
+    	this.employeeRepository.save(emp4);
     }
 }
